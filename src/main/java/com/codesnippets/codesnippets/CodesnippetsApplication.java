9cda95204638eefac21aad16cc084a85d9350ddb
unknown <kailash.gurjar187@gmail.com>
2020-09-16 20:12:18 +0530

    initial commit

---
+++ src/main/java/com/codesnippets/codesnippets/CodesnippetsApplication.java
@@ -0,0 +1,13 @@
+package com.codesnippets.codesnippets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodesnippetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodesnippetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codesnippets/codesnippets/CodesnippetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesnippets.codesnippets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodesnippetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
